b281bef7b1612f120b80247462898bb17b579e29
ajsaenz1091 <ajsaenz1091@gmail.com>
2020-11-12 20:37:43 -0500

    in App.js imported Table and Button components from reactstap, added a container class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Table, Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App container">
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Rating</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Book</td>
+              <td>2.5</td>
+              <td>
+                <Button color="success" size="sm">Edit</Button>
+                <Button color="danger" size="sm">Delete</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
